165fa3fe759d83db64ea0469560de0ca1a236db0
aginsun <arjen.hettinga@hotmail.com>
2013-01-01 23:36:15 +0100

    Update taleofkingdoms/core/handlers/SaveHandlerToK.java

--- taleofkingdoms/core/handlers/SaveHandlerToK.java
+++ taleofkingdoms/core/handlers/SaveHandlerToK.java
@@ -14,7 +14,6 @@ import net.minecraft.src.ModLoader;
  *  the IPlayerTracker class. 
  *  He deserves all the credits!
  */
-=======
 import cpw.mods.fml.common.IPlayerTracker;
 import aginsun.taleofkingdoms.core.GoldKeeper;
 
