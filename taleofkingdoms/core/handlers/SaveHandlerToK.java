1864a191086bd7b5340f64a72736ad16f7ab5124
aginsun <arjen.hettinga@hotmail.com>
2013-01-01 23:35:41 +0100

    update save Handler

--- taleofkingdoms/core/handlers/SaveHandlerToK.java
+++ taleofkingdoms/core/handlers/SaveHandlerToK.java
@@ -1,22 +1,13 @@
 package aginsun.taleofkingdoms.core.handlers;
 
-import aginsun.taleofkingdoms.core.DataStorage;
-import aginsun.taleofkingdoms.core.GoldKeeper;
-import cpw.mods.fml.common.FMLCommonHandler;
-import cpw.mods.fml.common.IPlayerTracker;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
-import java.io.*;
-import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.zip.GZIPInputStream;
-import java.util.zip.GZIPOutputStream;
-
-import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.CompressedStreamTools;
 import net.minecraft.nbt.NBTTagCompound;
+<<<<<<< HEAD
 import net.minecraft.src.ModLoader;
 
 /** File to save all the player data
@@ -24,85 +15,48 @@ import net.minecraft.src.ModLoader;
  *  the IPlayerTracker class. 
  *  He deserves all the credits!
  */
+=======
+import cpw.mods.fml.common.IPlayerTracker;
+import aginsun.taleofkingdoms.core.GoldKeeper;
+
+>>>>>>> update to save handler
 public class SaveHandlerToK implements IPlayerTracker
 {
 	public static int DataSaved = 1;
 	public static int Worthy;
 	private GoldKeeper gold;
 	private NBTTagCompound data;
-	public File playerdata;
 	
 	@Override
-	public void onPlayerLogin(EntityPlayer player)
+	public void onPlayerLogin(EntityPlayer receiver)
 	{
-		try
-		{
-			playerdata = new File(player.username + ".dat");
-			if(!playerdata.exists())
-			{
-				playerdata.createNewFile();
-				WriteToNBT();
-			}
-			ReadFromNBT();
-		}catch(Exception e){
-			e.printStackTrace();
-		}
+		getData(receiver);		
 	}
+	
 	@Override
-	public void onPlayerLogout(EntityPlayer player)
+	public void onPlayerLogout(EntityPlayer receiver)
 	{
-		try
-		{
-			playerdata = new File(player.username + ".dat");
-			WriteToNBT();
-		}catch(Exception e){
-			e.printStackTrace();
-		}
+		SetData(receiver);
 	}
 	@Override
 	public void onPlayerChangedDimension(EntityPlayer player){}
 	@Override
 	public void onPlayerRespawn(EntityPlayer player){}
 	
-	public void WriteToNBT()
+	public void SetData(EntityPlayer player)
 	{
-        try
-        {
-        	if(playerdata != null)
-        	{
-        		FileOutputStream fileoutputstream = new FileOutputStream(playerdata);
-        		NBTTagCompound nbt = new NBTTagCompound();
-        	
-        		nbt.setInteger("Worthy", Worthy);
-        		nbt.setInteger("GoldTotal", gold.GoldTotal);
-        	
-        		CompressedStreamTools.writeCompressed(nbt, fileoutputstream);
-        		fileoutputstream.close();
-        	}
-        }catch(Exception e){
-        	e.printStackTrace();
-        }
+		data = player.getEntityData().getCompoundTag(player.PERSISTED_NBT_TAG);
+		data.setBoolean("mute", true);
+		data.setInteger("GoldTotal", gold.GoldTotal);
+		player.getEntityData().setCompoundTag(player.PERSISTED_NBT_TAG, data);
 	}
 	
-	public void ReadFromNBT()
+	public void getData(EntityPlayer player)
 	{
-		try
+		data = player.getEntityData().getCompoundTag(player.PERSISTED_NBT_TAG);
+		if(data.hasKey("GoldTotal"))
 		{
-			if(playerdata != null)
-			{
-				FileInputStream fileinputstream = new FileInputStream(playerdata);
-				NBTTagCompound nbt = CompressedStreamTools.readCompressed(fileinputstream);
-				if(nbt.hasKey("Worthy"))
-				{
-					Worthy = nbt.getInteger("Worthy");
-				}
-				if(nbt.hasKey("GoldTotal"))
-				{
-					gold.GoldTotal = nbt.getInteger("GoldTotal");
-				}
-			}
-		}catch(Exception e){
-				e.printStackTrace();
+			gold.GoldTotal = data.getInteger("GoldTotal");
 		}
 	}
 }
